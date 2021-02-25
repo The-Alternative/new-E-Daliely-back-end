25eaf958b4531e7003dc011588adc70da954ecd6
fahed-92 <fahed9285@gmail.com>
2021-02-25 11:50:47 +0200

    fahed

--- routes/api.php
+++ routes/api.php
@@ -19,29 +19,30 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 /*_____________ Product routes _____________*/
+
 Route::group(['middleware' =>'api','prefix'=>'products','namespace'=>'Product'],function(){
-        Route::GET('/get','ProductsController@get');
-        Route::GET('/getById/{id}','ProductsController@getById');
-        Route::POST('/create','ProductsController@create');
-        Route::PUT('/update/{id}','ProductsController@update');
-        Route::GET('/search/{title}','ProductsController@search');
-        Route::PUT('/trash/{id}','ProductsController@trash');
-        Route::PUT('/restoreTrashed/{id}','ProductsController@restoreTrashed');
-        Route::GET('/getTrashed','ProductsController@getTrashed');
-        Route::DELETE('/delete/{id}','ProductsController@delete');
+                Route::GET('/get','ProductsController@get');
+                Route::GET('/getById/{id}','ProductsController@getById');
+                Route::POST('/create','ProductsController@create');
+                Route::PUT('/update/{id}','ProductsController@update');
+                Route::GET('/search/{title}','ProductsController@search');
+                Route::PUT('/trash/{id}','ProductsController@trash');
+                Route::PUT('/restoreTrashed/{id}','ProductsController@restoreTrashed');
+                Route::GET('/getTrashed','ProductsController@getTrashed');
+                Route::DELETE('/delete/{id}','ProductsController@delete');
 });
 
 /*_____________Category routes_____________*/
 Route::group(['middleware' =>'api','prefix'=>'categories','namespace'=>'Category'],function(){
-        Route::GET('/get','CategoriesController@get');
-        Route::GET('/getById/{id}','CategoriesController@getById');
-        Route::POST('/create','CategoriesController@create');
-        Route::PUT('/update','CategoriesController@update');
-        Route::PUT('/trash/{id}','CategoriesController@trash');
-        Route::PUT('/restoreTrashed/{id}','CategoriesController@restoreTrashed');
-        Route::GET('/search/{name}','CategoriesController@search');
-        Route::GET('/getTrashed','CategoriesController@getTrashed');
-        Route::DELETE('/delete/{id}','CategoriesController@delete');
+                Route::GET('/get','CategoriesController@get');
+                Route::GET('/getById/{id}','CategoriesController@getById');
+                Route::POST('/create','CategoriesController@create');
+                Route::PUT('/update','CategoriesController@update');
+                Route::PUT('/trash/{id}','CategoriesController@trash');
+                Route::PUT('/restoreTrashed/{id}','CategoriesController@restoreTrashed');
+                Route::GET('/search/{name}','CategoriesController@search');
+                Route::GET('/getTrashed','CategoriesController@getTrashed');
+                Route::DELETE('/delete/{id}','CategoriesController@delete');
 });
 
 
